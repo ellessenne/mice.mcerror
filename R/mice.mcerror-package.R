8276269bd032ca8c363cebd1795eac4e0fc50b86
Alessandro Gasparini <git@ellessenne.xyz>
2022-05-03 14:09:30 +0200

    Package init

---
+++ R/mice.mcerror-package.R
@@ -0,0 +1,10 @@
+#' @title Calculate Monte Carlo Error Estimates for Statistics Computed Using Multiply Imputed Data
+#' @description Calculate Monte Carlo error estimates for statistics computed by pooling multiply imputed data. This package is an add-on to the {mice} package, which is thus required.
+#' @name mice.mcerror_package
+#' @docType package
+#' @author Alessandro Gasparini (alessandro@@ellessenne.xyz)
+#' @import mice
+NULL
+
+# Quiets concerns of R CMD check re: variable names used internally
+# if (getRversion() >= "2.15.1") utils::globalVariables(c("..."))
